0cab4d4c211e637cda3810b68b6ca360a57fb2b2
Manthan Ankolekar <manthan.ank46@gmail.com>
2024-10-17 14:56:45 +0530

    Update package.json version and add custom directives to public API

--- projects/custom-directives/src/public-api.ts
+++ projects/custom-directives/src/public-api.ts
@@ -4,3 +4,9 @@
 
 export * from './lib/custom-directives.service';
 export * from './lib/custom-directives.component';
+export * from './lib/projects/customDirectives/src/lib/custom-class.directive';
+export * from './lib/projects/customDirectives/src/lib/custom-if.directive';
+export * from './lib/projects/customDirectives/src/lib/custom-style.directive';
+export * from './lib/projects/customDirectives/src/lib/custom-for.directive';
+export * from './lib/projects/customDirectives/src/lib/custom-model.directive';
+export * from './lib/projects/customDirectives/src/lib/custom-switch.directive';
